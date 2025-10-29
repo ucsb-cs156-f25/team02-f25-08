f0fb4a9399cac73122f539d0ecba936753d74fbd
Deng Lucy <siyudeng@ucsb.edu>
2025-10-29 16:33:09 -0700

    create fixtures for ucsb org

---
+++ frontend/src/fixtures/UCSBOrganizationFixtures.js
@@ -0,0 +1,34 @@
+const UCSBOrganizationFixtures = {
+    oneUCSBOrganization: {
+        "id": 1,
+        "orgCode": "ZPR",
+        "orgTranslationShort": "ZETA PHI RHO",
+        "orgTranslation": "ZETA PHI RHO",
+        "inactive": false
+    },
+    threeUCSBOrganizations: [
+        {
+            "id": 1,
+            "orgCode": "ZPR",
+            "orgTranslationShort": "ZETA PHI RHO",
+            "orgTranslation": "ZETA PHI RHO",
+            "inactive": false
+        },
+        {
+            "id": 2,
+            "orgCode": "SKY",
+            "orgTranslationShort": "SKYDIVING CLUB",
+            "orgTranslation": "SKYDIVING CLUB AT UCSB",
+            "inactive": false
+        },
+        {
+            "id": 3,
+            "orgCode": "KRC",
+            "orgTranslationShort": "KOREAN RADIO CL",
+            "orgTranslation": "KOREAN RADIO CLUB",
+            "inactive": false
+        }
+    ]
+};
+
+export { UCSBOrganizationFixtures };
