a88c2b9c0f4b844d6bfb356c6938c9fce82ecbb7
Dalia Sebat <daliasebat@ucsb.edu>
2025-10-29 19:49:03 -0700

    added Articles fixtures

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,38 @@
+const articlesFixtures = {
+    oneDate: {
+        id: 1,
+        title: "Article 1",
+        url: "https://article1.com",
+        explanation: "Article about article 1",
+        email: "daliasebat1@gmail.com",
+        dateAdded: "2022-01-02T12:00:00",
+    },
+    threeDates: [
+        {
+            id: 1,
+            title: "Article 1",
+            url: "https://article1.com",
+            explanation: "Article about article 1",
+            email: "daliasebat1@gmail.com",
+            dateAdded: "2022-01-02T12:00:00",
+        },
+        {
+            id: 2,
+            title: "Article 2",
+            url: "https://article2.com",
+            explanation: "Article about article 2",
+            email: "daliasebat2@gmail.com",
+            dateAdded: "2022-04-03T12:00:00",
+        },
+        {
+            id: 3,
+            title: "Article 3",
+            url: "https://article3.com",
+            explanation: "Article about article 3",
+            email: "daliasebat3@gmail.com",
+            dateAdded: "2022-07-04T12:00:00",
+        },
+    ],
+};
+
+export { articlesFixtures };
