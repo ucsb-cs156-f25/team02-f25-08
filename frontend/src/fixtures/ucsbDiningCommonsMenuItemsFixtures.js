db07eb5fdeef19b066e037a11fc1d541efcb77a3
Emily Lao <emilylao@ucsb.edu>
2025-11-03 22:34:12 -0800

    added DiningCommonsMenuItems fixtures

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemsFixtures.js
@@ -0,0 +1,30 @@
+const ucsbDiningCommonsMenuItemsFixtures = {
+  oneDate: {
+    id: 1,
+    diningCommonsCode: "Ortega",
+    name: "Pesto Pasta",
+    station: "Take Out",
+  },
+  threeDates: [
+    {
+      id: 1,
+      diningCommonsCode: "Portola",
+      name: "Sushi",
+      station: "International",
+    },
+    {
+      id: 2,
+      diningCommonsCode: "De La Guerra",
+    name: "Tacos",
+    station: "Western",
+    },
+    {
+      id: 3,
+      diningCommonsCode: "Carillo",
+    name: "Granola",
+    station: "Brunch",
+    },
+  ],
+};
+
+export { ucsbDiningCommonsMenuItemsFixtures };
