a7ae669ac36b76f5ba77c6da6090895a9db50158
Varsha Narasiman <vnarasiman@umail.ucsb.edu>
2025-11-06 22:21:23 -0800

    vn - changed duplicate HelpRequests imports

--- frontend/src/tests/pages/HelpRequests/HelpRequestsCreatePage.test.jsx
+++ frontend/src/tests/pages/HelpRequests/HelpRequestsCreatePage.test.jsx
@@ -1,48 +1,107 @@
-import { render, screen } from "@testing-library/react";
-import HelpRequestsCreatePage from "main/pages/HelpRequests/HelpRequestsCreatePage";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { MemoryRouter } from "react-router";
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import HelpRequestsCreatePage from 'main/pages/HelpRequests/HelpRequestsCreatePage';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { MemoryRouter } from 'react-router';
 
-import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
-import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
-import axios from "axios";
-import AxiosMockAdapter from "axios-mock-adapter";
-import { expect } from "vitest";
+import { apiCurrentUserFixtures } from 'fixtures/currentUserFixtures';
+import { systemInfoFixtures } from 'fixtures/systemInfoFixtures';
 
-describe("HelpRequestsCreatePage tests", () => {
+import axios from 'axios';
+import AxiosMockAdapter from 'axios-mock-adapter';
+
+const mockToast = vi.fn();
+vi.mock('react-toastify', async (importOriginal) => {
+  const originalModule = await importOriginal();
+  return {
+    ...originalModule,
+    toast: vi.fn((x) => mockToast(x)),
+  };
+});
+
+const mockNavigate = vi.fn();
+vi.mock('react-router', async (importOriginal) => {
+  const originalModule = await importOriginal();
+  return {
+    ...originalModule,
+    Navigate: vi.fn((x) => {
+      mockNavigate(x);
+      return null;
+    }),
+  };
+});
+
+describe('HelpRequestsCreatePage tests', () => {
   const axiosMock = new AxiosMockAdapter(axios);
 
-  const setupUserOnly = () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
     axiosMock.reset();
     axiosMock.resetHistory();
-    axiosMock
-      .onGet("/api/currentUser")
-      .reply(200, apiCurrentUserFixtures.userOnly);
-    axiosMock
-      .onGet("/api/systemInfo")
-      .reply(200, systemInfoFixtures.showingNeither);
-  };
+    axiosMock.onGet('/api/currentUser').reply(200, apiCurrentUserFixtures.userOnly);
+    axiosMock.onGet('/api/systemInfo').reply(200, systemInfoFixtures.showingNeither);
+  });
 
   const queryClient = new QueryClient();
-  test("Renders expected content", async () => {
-    // arrange
-
-    setupUserOnly();
-
-    // act
+  test('renders without crashing', async () => {
     render(
       <QueryClientProvider client={queryClient}>
         <MemoryRouter>
           <HelpRequestsCreatePage />
         </MemoryRouter>
-      </QueryClientProvider>,
+      </QueryClientProvider>
     );
 
-    // assert
+    await waitFor(() => {
+      expect(screen.getByLabelText('Name')).toBeInTheDocument();
+    });
+  });
 
-    await screen.findByText("Create page not yet implemented");
-    expect(
-      screen.getByText("Create page not yet implemented"),
-    ).toBeInTheDocument();
+  test('on submit, makes request to backend, and redirects to /restaurants', async () => {
+    const queryClient = new QueryClient();
+    const restaurant = {
+      id: 3,
+      name: 'South Coast Deli',
+      description: 'Sandwiches and Salads',
+    };
+
+    axiosMock.onPost('/api/restaurants/post').reply(202, restaurant);
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <HelpRequestsCreatePage />
+        </MemoryRouter>
+      </QueryClientProvider>
+    );
+
+    await waitFor(() => {
+      expect(screen.getByLabelText('Name')).toBeInTheDocument();
+    });
+
+    const nameInput = screen.getByLabelText('Name');
+    expect(nameInput).toBeInTheDocument();
+
+    const descriptionInput = screen.getByLabelText('Description');
+    expect(descriptionInput).toBeInTheDocument();
+
+    const createButton = screen.getByText('Create');
+    expect(createButton).toBeInTheDocument();
+
+    fireEvent.change(nameInput, { target: { value: 'South Coast Deli' } });
+    fireEvent.change(descriptionInput, {
+      target: { value: 'Sandwiches and Salads' },
+    });
+    fireEvent.click(createButton);
+
+    await waitFor(() => expect(axiosMock.history.post.length).toBe(1));
+
+    expect(axiosMock.history.post[0].params).toEqual({
+      name: 'South Coast Deli',
+      description: 'Sandwiches and Salads',
+    });
+
+    // assert - check that the toast was called with the expected message
+    expect(mockToast).toBeCalledWith('New restaurant Created - id: 3 name: South Coast Deli');
+    expect(mockNavigate).toBeCalledWith({ to: '/restaurants' });
   });
 });
