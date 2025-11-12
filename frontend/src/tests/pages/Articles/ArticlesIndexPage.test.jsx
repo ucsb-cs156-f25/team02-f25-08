6e6615600cb89ea29272122182351c729bebb964
Varsha Narasiman <vnarasiman@umail.ucsb.edu>
2025-11-12 11:32:18 -0800

    vn - fixed formatting issue

--- frontend/src/tests/pages/Articles/ArticlesIndexPage.test.jsx
+++ frontend/src/tests/pages/Articles/ArticlesIndexPage.test.jsx
@@ -9,42 +9,42 @@ import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
 
 describe("ArticlesIndexPage tests", () => {
-    const axiosMock = new AxiosMockAdapter(axios);
+  const axiosMock = new AxiosMockAdapter(axios);
 
-    const setupUserOnly = () => {
-        axiosMock.reset();
-        axiosMock.resetHistory();
-        axiosMock
-            .onGet("/api/currentUser")
-            .reply(200, apiCurrentUserFixtures.userOnly);
-        axiosMock
-            .onGet("/api/systemInfo")
-            .reply(200, systemInfoFixtures.showingNeither);
-    };
+  const setupUserOnly = () => {
+    axiosMock.reset();
+    axiosMock.resetHistory();
+    axiosMock
+      .onGet("/api/currentUser")
+      .reply(200, apiCurrentUserFixtures.userOnly);
+    axiosMock
+      .onGet("/api/systemInfo")
+      .reply(200, systemInfoFixtures.showingNeither);
+  };
 
-    const queryClient = new QueryClient();
-    test("Renders expected content", async () => {
-        // arrange
+  const queryClient = new QueryClient();
+  test("Renders expected content", async () => {
+    // arrange
 
-        setupUserOnly();
+    setupUserOnly();
 
-        // act
+    // act
 
-        render(
-            <QueryClientProvider client={queryClient}>
-                <MemoryRouter>
-                    <ArticlesIndexPage />
-                </MemoryRouter>
-            </QueryClientProvider>,
-        );
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <ArticlesIndexPage />
+        </MemoryRouter>
+      </QueryClientProvider>,
+    );
 
-        await screen.findByText("Index page not yet implemented");
+    await screen.findByText("Index page not yet implemented");
 
-        // assert
-        expect(
-            screen.getByText("Index page not yet implemented"),
-        ).toBeInTheDocument();
-        expect(screen.getByText("Create")).toBeInTheDocument();
-        expect(screen.getByText("Edit")).toBeInTheDocument();
-    });
+    // assert
+    expect(
+      screen.getByText("Index page not yet implemented"),
+    ).toBeInTheDocument();
+    expect(screen.getByText("Create")).toBeInTheDocument();
+    expect(screen.getByText("Edit")).toBeInTheDocument();
+  });
 });
