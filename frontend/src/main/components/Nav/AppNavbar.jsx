99212550e766e345e01a60133e1ace2c9042d107
DengLucy <156357269+DengLucy@users.noreply.github.com>
2025-11-12 23:44:27 -0800

    Update AppNavbar.jsx

--- frontend/src/main/components/Nav/AppNavbar.jsx
+++ frontend/src/main/components/Nav/AppNavbar.jsx
@@ -68,6 +68,9 @@ export default function AppNavbar({
                   <Nav.Link as={Link} to="/ucsbdates">
                     UCSB Dates
                   </Nav.Link>
+                  <Nav.Link as={Link} to="/ucsborganizations">
+                    UCSBOrganizations
+                  </Nav.Link>
                   <Nav.Link as={Link} to="/recommendationrequest">
                     Recommendation Request
                   </Nav.Link>
